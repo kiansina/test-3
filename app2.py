113304b5121902923f3abba6567f4a0c9b67d10c
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-08-24 13:41:45 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,6 +4,25 @@ import random
 import time
 from google.oauth2 import service_account
 from gspread_pandas import Spread,Client
+from PIL import Image
+
+img=Image.open('lo.jfif')
+st.set_page_config(page_title="Questionnaire", page_icon=img)
+
+hide_menu_style= """
+          <style>
+          #MainMenu {visibility: hidden; }
+          footer {visibility: hidden;}
+          </style>
+          """
+#Image_logo="""
+#           <p>
+#           <img src="2.PNG" width="200" height="267"/>
+#           </p>
+#           """
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+#vst.markdown(Image_logo, unsafe_allow_html=True)
 
 scope=["https://www.googleapis.com/auth/drive",
     "https://www.googleapis.com/auth/spreadsheets"]
@@ -94,6 +113,18 @@ if "usercheck" not in st.session_state:
 if "rn" not in st.session_state:
     st.session_state["rn"] = random.sample(range(1, 10), 5)
 
+
+@st.experimental_singleton
+def ran():
+    for i in range(0,len(st.session_state["rn"])):
+        st.session_state["ch{}".format(i)]=random.sample(choices[str(st.session_state["rn"][i])],k=len(choices[str(st.session_state["rn"][i])]))
+        st.session_state["cho{}".format(i)]=[x[0] for x in st.session_state["ch{}".format(i)]]
+        st.session_state["che{}".format(i)]=[x[1] for x in st.session_state["ch{}".format(i)]]
+    return st.session_state["ch0"], st.session_state["ch1"] ,st.session_state["ch2"] ,st.session_state["ch3"] ,st.session_state["ch4"] ,st.session_state["cho0"], st.session_state["cho1"] , st.session_state["cho2"] , st.session_state["cho3"] , st.session_state["cho4"] , st.session_state["che0"] , st.session_state["che1"], st.session_state["che2"], st.session_state["che3"], st.session_state["che4"]
+
+st.session_state["ch0"], st.session_state["ch1"] ,st.session_state["ch2"] ,st.session_state["ch3"] ,st.session_state["ch4"] ,st.session_state["cho0"],st.session_state["cho1"] , st.session_state["cho2"] , st.session_state["cho3"] , st.session_state["cho4"] , st.session_state["che0"] , st.session_state["che1"],st.session_state["che2"], st.session_state["che3"], st.session_state["che4"]=ran()
+
+
 if check_password():
     @st.cache(allow_output_mutation=True)
     def get_data():
@@ -113,19 +144,20 @@ if check_password():
             Nome = st.text_input("Nome:")
             Cognome = st.text_input("Cognome:")
             sodisfazione = st.slider("Sodisfazione", 0, 100)
-            Qa=st.radio(questions[str(st.session_state["rn"][0])],random.sample([x[0] for x in choices[str(st.session_state["rn"][0])]],k=len([x[0] for x in choices[str(st.session_state["rn"][0])]])))
-            Qb=st.text_input(questions[str(st.session_state["rn"][1])])
-            Qc=st.text_input(questions[str(st.session_state["rn"][2])])
-            Qd=st.text_input(questions[str(st.session_state["rn"][3])])
-            Qe=st.text_input(questions[str(st.session_state["rn"][4])])
+            Qa=st.radio("1)    "+questions[str(st.session_state["rn"][0])],st.session_state["cho0"],horizontal=False)
+            Qb=st.radio("2)    "+questions[str(st.session_state["rn"][1])],st.session_state["cho1"],horizontal=False)
+            Qc=st.radio("3)    "+questions[str(st.session_state["rn"][2])],st.session_state["cho2"],horizontal=False)
+            Qd=st.radio("4)    "+questions[str(st.session_state["rn"][3])],st.session_state["cho3"],horizontal=False)
+            Qe=st.radio("5)    "+questions[str(st.session_state["rn"][4])],st.session_state["cho4"],horizontal=False)
             if st.button("Submit"):
                 get_data().append({"Username":Username,"Nome": Nome,"Cognome":Cognome, "Livello sodisfazione": sodisfazione, "q1": Qa, "q2": Qb, "q3": Qc, "q4": Qd, "q5": Qe, "time":time.time()-st.session_state["t0"]})
                 A=pd.DataFrame(get_data())
+                st.session_state["B"]=pd.DataFrame({"Username":Username,"Nome": Nome,"Cognome":Cognome, "Livello sodisfazione": sodisfazione, "q1": st.session_state["che0"][st.session_state["cho0"].index(Qa)], "q2": st.session_state["che1"][st.session_state["cho1"].index(Qb)], "q3": st.session_state["che2"][st.session_state["cho2"].index(Qc)], "q4": st.session_state["che3"][st.session_state["cho3"].index(Qd)], "q5": st.session_state["che4"][st.session_state["cho4"].index(Qe)], "time":time.time()-st.session_state["t0"]},index=[0])
                 st.write(A)
             st.write('Se Lei è sicuro da chiudere l\'esamae, premi conferma')
             if st.button("Confirm"):
                 L=len(pd.DataFrame(get_data()))
-                dx=df.append(pd.DataFrame(get_data()).loc[L-1,:],ignore_index=True)
+                dx=df.append(st.session_state["B"].loc[L-1,:],ignore_index=True)
                 s.df_to_sheet(dx,sheet='test',index=False)
                 st.title('la sua esame è finito 😊.')
                 st.title("Grazie per la collaborazione! 😍")
